b9dc5a368201aa5f5b278ad65e30d71bcfee8c50
Marvin Valke <valkemarvin@gmail.com>
2021-12-09 11:52:09 +0100

    added profile routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,4 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+
+const UserModel = require("../models/User.model.js");
+
+// USER CAN GO TO HIS PROFILE
+router.get("/profile", (req, res) => {
+    const profileId = req.session.loggedInUser._id;
+    UserModel.findById(profileId)
+      .populate("MissionsAdded")
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: "Something went wrong",
+          message: err,
+        });
+      });
+  });
+
+
+  // USER CAN EDIT HIS PROFILE
+  router.patch('/profile/edit' , (req,res,next) =>{
+    const profileId = req.session.loggedInUser._id;
+    const {username, email, passwordHash, profilePic} = req.body
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(passwordHash, salt);
+    let editedProfile = {
+        username,
+        email,
+        passwordHash:hash,
+    }
+    if (profilePic) {
+        editedProfile.profilePic = profilePic;
+      }
+    UserModel.findByIdAndUpdate(profileId, {$set:editedProfile}, {new: true})
+  })
+  
+
+  // USER CAN DELETE HIS PROFILE
+  router.delete("/profile/delete", (req, res, next) => {
+    const profileId = req.session.loggedInUser._id;
+    UserModel.findByIdAndDelete(profileId)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: "Something went wrong",
+          message: err,
+        });
+      });
+  })
+
+// USER CAN ACCESS THE MISSIONS HE APPLYED FOR
+router.get("/profile/missions", (req, res) => {
+    const profileId = req.session.loggedInUser._id;
+    UserModel.findById(profileId)
+      .populate("MissionsAdded")
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: "Something went wrong",
+          message: err,
+        });
+      });
+  });
+
 
 module.exports = router;
