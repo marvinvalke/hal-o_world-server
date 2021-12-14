62d13370e62f5d097dda0e7d272a4f26e23958e4
Marvin Valke <valkemarvin@gmail.com>
2021-12-14 11:43:39 +0100

    tuesdayy commits

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ app.use('/api', nasaRoutes)
 // const aboutRoutes = require('./routes/about.routes')
 // app.use("/api", aboutRoutes);
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
